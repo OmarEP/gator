5bc9fc397a88c3ce36541c12483d14478d27f196
OmarEP <omrspz@gmail.com>
2024-12-19 00:51:39 -0500

    add middleware

---
+++ handler_follow.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/OmarEP/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerFollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <feed_url>", cmd.Name)
+	}
+
+	url := cmd.Args[0]
+
+	feed, err := s.db.GetFeedFromUrl(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	feed_follow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+
+	fmt.Println("Feed follow created:")
+	printFeedFollow(feed_follow.UserName, feed_follow.FeedName)
+	return nil
+}
+
+func handlerListFeedFollows(s *state, cmd command, user database.User) error {
+	feed_follows, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+	if err != nil {
+		return err
+	}
+
+	if len(feed_follows) == 0 {
+		fmt.Println("No feed follows found for this user.")
+		return nil
+	}
+
+	fmt.Printf("Feed follows for user %s:\n", user.Name)
+	for _, feed_follow := range feed_follows {
+		fmt.Printf("* %s\n", feed_follow.FeedName)
+	}
+
+	return nil
+}
+
+func printFeedFollow(username, feedname string) {
+	fmt.Printf("* User:			%s\n", username)
+	fmt.Printf("* Feed:			%s\n", feedname)
+}
+
